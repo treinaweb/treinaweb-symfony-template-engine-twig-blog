ae77d5a528c3beef5e86d52d3e33236f9ad2f411
elton869 <elton869@gmail.com>
2019-09-14 13:21:51 +0000

    09 - loop for

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -17,12 +17,25 @@ class PostController
         $content = $twig->render('post/index.html.twig', [
             'curso' => 'Java',
             'escola' => 'Treinaweb',
-            'post'   => [
-                'titulo' => 'meu post',
-                'conteudo' => 'meu conteudo',
-                'data'      => '10/09/2019'
+            'posts'   => [
+                0 => [
+                    'titulo' => 'meu post1',
+                    'conteudo' => 'meu conteudo1',
+                    'data'      => '10/09/2019'
+                ],
+                1 => [
+                    'titulo' => 'meu post2',
+                    'conteudo' => 'meu conteudo2',
+                    'data'      => '10/09/2019'
+                ],
+                2 => [
+                    'titulo' => 'meu post3',
+                    'conteudo' => 'meu conteudo3',
+                    'data'      => '10/09/2019'
+                ],
             ],
-            'codigo'    => '<script>alert("teste sendo exibido")</script>'
+            'codigo'    => '<script>alert("teste sendo exibido")</script>',
+            'numeros'   => [2, 4, 5, 8]
         ]);
 
         return new Response($content);
